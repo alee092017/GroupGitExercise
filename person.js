e8fc7d35439d4d081ba0aa7b67a682b69684921b
bart369 <bartinpeguero@gmail.com>
2017-11-13 16:03:17 -0500

    merging soon

---
+++ person.js
@@ -0,0 +1,16 @@
+class Person {
+    constructor(options) {
+        this.name = options.name || 'John Smith';
+        this.age = options.age;
+        this.gender = options.gender;
+    }
+
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
+        console.log(`My gender is ${this.gender}`);
+    }
+}
+
+var people = [];
+
+//Merging this badboy NOW
